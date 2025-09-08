6e42703ba953b335848d31e3e81733c885364186
Patrick Lund Haugen <patrilha@uio.no>
2025-09-08 13:50:24 +0200

    altered carousel

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { cn } from "../../lib/utils"
 
 type Variant = "default" | "secondary" | "outline";
 type Size = "default" | "sm" | "icon" | "link";
@@ -36,7 +37,7 @@ export function Button({
                        }: ButtonProps) {
     return (
         <button
-            className={`${baseStyles} ${variants[variant]} ${sizes[size]} ${className}`}
+            className={cn(baseStyles, variants[variant], sizes[size], className)}
             {...props}
         >
             {children}
--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -3,10 +3,8 @@
 import * as React from "react"
 import useEmblaCarousel, { type UseEmblaCarouselType } from "embla-carousel-react"
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { cn } from "../../lib/utils"
 import { Button } from "./button"
-
-const cn = (...classes: (string | boolean | undefined)[]) =>
-    classes.filter(Boolean).join(" ");
 
 type CarouselApi = UseEmblaCarouselType[1]
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>
@@ -37,7 +35,6 @@ function useCarousel() {
     if (!context) {
         throw new Error("useCarousel must be used within a <Carousel />")
     }
-
     return context
 }
 
@@ -88,20 +85,19 @@ function Carousel({
     )
 
     React.useEffect(() => {
-        if (!api || !setApi) return
-        setApi(api)
-    }, [api, setApi])
-
-    React.useEffect(() => {
         if (!api) return
+        if (setApi) setApi(api)
+
         onSelect(api)
+
         api.on("reInit", onSelect)
         api.on("select", onSelect)
 
         return () => {
             api?.off("select", onSelect)
+            api?.off("reInit", onSelect)
         }
-    }, [api, onSelect])
+    }, [api, setApi, onSelect])
 
     return (
         <CarouselContext.Provider
@@ -184,7 +180,7 @@ function CarouselPrevious({
             data-slot="carousel-previous"
             variant={variant}
             className={cn(
-                "!absolute !size-8 !rounded-full",
+                "absolute size-8 rounded-full",
                 orientation === "horizontal"
                     ? "top-1/2 left-2 md:-left-12 -translate-y-1/2"
                     : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -213,7 +209,7 @@ function CarouselNext({
             data-slot="carousel-next"
             variant={variant}
             className={cn(
-                "!absolute !size-8 !rounded-full",
+                "absolute size-8 rounded-full",
                 orientation === "horizontal"
                     ? "top-1/2 right-2 md:-right-12 -translate-y-1/2"
                     : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
