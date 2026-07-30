84d415a72f5f3d1db306155f7b42c2bbc4e949df
Patrick Lund Haugen <patrilha@uio.no>
2026-07-30 17:07:51 +0200

    final video preview polish

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,26 +8,30 @@ export default function Home() {
     const { t } = useTranslation();
 
     return (
-        <section className="relative h-full flex flex-col justify-end md:grid md:grid-cols-2 md:items-end gap-8 lg:gap-32">
-            <VideoPreview />
-            <div className="animate-left-col flex flex-col">
-                <p className="text-xs md:text-sm font-medium tracking-tight uppercase">
-                    {t("home.subheading")}
-                </p>
-                <div className="flex flex-col sm:flex-row sm:gap-x-2 text-3xl lg:text-4xl font-bold tracking-tight uppercase">
-                    <span>{t("home.firstName")}</span>
-                    <span>{t("home.lastName")}</span>
-                </div>
-                <p className="md:text-lg font-semibold tracking-tight leading-tight mt-4">
-                    {t("home.about")}
-                </p>
-                <Footer />
+        <section className="h-full flex flex-col gap-8">
+            <div className="flex-1 flex items-center justify-center">
+                <VideoPreview />
             </div>
-            <div className="w-full lg:justify-self-end">
-                <p className="text-xs md:text-sm font-medium tracking-tight uppercase mb-3">
-                    {t("work.recentWorkTitle")}
-                </p>
-                <Accordion />
+            <div className="flex flex-col justify-end md:grid md:grid-cols-2 md:items-end gap-8 lg:gap-32">
+                <div className="animate-left-col flex flex-col">
+                    <p className="text-xs md:text-sm font-medium tracking-tight uppercase">
+                        {t("home.subheading")}
+                    </p>
+                    <div className="flex flex-col sm:flex-row sm:gap-x-2 text-3xl lg:text-4xl font-bold tracking-tight uppercase">
+                        <span>{t("home.firstName")}</span>
+                        <span>{t("home.lastName")}</span>
+                    </div>
+                    <p className="md:text-lg font-semibold tracking-tight leading-tight mt-4">
+                        {t("home.about")}
+                    </p>
+                    <Footer />
+                </div>
+                <div className="w-full lg:justify-self-end">
+                    <p className="text-xs md:text-sm font-medium tracking-tight uppercase mb-3">
+                        {t("work.recentWorkTitle")}
+                    </p>
+                    <Accordion />
+                </div>
             </div>
         </section>
     );
